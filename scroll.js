64491b51aa7ca6225658d57e300f9519e947e2de
maruhsilva <marua.desenvolvedor@gmail.com>
2024-06-10 20:21:34 -0300

    teste

---
+++ scroll.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.titulo', {duration: 2000});
+sr.reveal('.atendimento', {duration: 2000});
+sr.reveal('.atendimento1', {duration: 2000});
+sr.reveal('.sobre', {duration: 2000});
+sr.reveal('.titulo1', {duration: 2000});
+sr.reveal('.contato', {duration: 2000});
+sr.reveal('.container', {duration: 2000});
